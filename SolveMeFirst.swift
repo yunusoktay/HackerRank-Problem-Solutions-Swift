3a6df7f8522b45c1e03a4acee20b8747faea28f7
yunusoktay <ynsokty.3@gmail.com>
2022-08-17 11:56:24 +0300

    solution

---
+++ SolveMeFirst.swift
@@ -0,0 +1,17 @@
+//
+//  SolveMeFirst.swift
+//  
+//
+//  Created by yunus oktay on 17.08.2022.
+//
+
+import Foundation
+
+// read integers line by line
+
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+ 
+var c = a + b
+print(c)
+// Hint: Type print(a + b) below
