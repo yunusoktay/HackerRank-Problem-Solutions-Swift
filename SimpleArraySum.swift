50636ec2ce5c94d36a46edef32a1f81a8ae100f0
yunusoktay <ynsokty.3@gmail.com>
2022-08-17 12:09:08 +0300

    solution

---
+++ SimpleArraySum.swift
@@ -0,0 +1,45 @@
+//
+//  SimpleArraySum.swift
+//  
+//
+//  Created by yunus oktay on 17.08.2022.
+//
+
+import Foundation
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+func simpleArraySum(ar: [Int]) -> Int {
+    var result = 0
+    for num in ar {
+        result += num
+    }
+    return result
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let arCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let ar: [Int] = arTemp.split(separator: " ").map {
+    if let arItem = Int($0) {
+        return arItem
+    } else { fatalError("Bad input") }
+}
+
+guard ar.count == arCount else { fatalError("Bad input") }
+
+let result = simpleArraySum(ar: ar)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
