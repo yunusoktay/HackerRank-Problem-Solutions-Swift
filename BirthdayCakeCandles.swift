50e73d5451b8db91cf91a4bf24495e41a1937e93
yunusoktay <ynsokty.3@gmail.com>
2022-08-26 13:51:30 +0300

    solution

---
+++ BirthdayCakeCandles.swift
@@ -0,0 +1,53 @@
+//
+//  BirthdayCakeCandles.swift
+//  
+//
+//  Created by yunus oktay on 26.08.2022.
+//
+
+import Foundation
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+func birthdayCakeCandles(candles: [Int]) -> Int {
+    var ans = 0
+       var tallest = Int.min
+
+       for a in candles {
+           if tallest < a {
+               tallest = a
+               ans = 1
+           } else if tallest == a {
+               ans += 1
+           }
+       }
+       return ans
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let candlesCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let candlesTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let candles: [Int] = candlesTemp.split(separator: " ").map {
+    if let candlesItem = Int($0) {
+        return candlesItem
+    } else { fatalError("Bad input") }
+}
+
+guard candles.count == candlesCount else { fatalError("Bad input") }
+
+let result = birthdayCakeCandles(candles: candles)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
